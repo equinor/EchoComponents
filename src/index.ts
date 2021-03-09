6be65f831f7ea01ee0aef28fbda96e81363941cb
jankondula <jank@equinor.com>
2021-03-09 12:44:47 +0100

    Updated index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+//components
 export { ButtonWithPopover } from './components/tags/buttonWithPopover/ButtonWithPopover';
 export { ContextMenu } from './components/tags/contextMenu/ContextMenu';
 export { DataInfoButton } from './components/tags/contextMenuPopover/DataInfoButton';
 export { TagContextMenu } from './components/tags/tagContextMenu/TagContextMenu';
-export { Icon } from './elements/icons/icon/Icon';
-export { TagIcon } from './elements/icons/tagIcon/TagIcon';
-export { TagIconShadowWrapper } from './elements/icons/tagIcon/TagIconShadow';
+export { DialogGenerator } from './components/ui/dialogGenerator/DialogGenerator';
+export { ListRow } from './components/ui/listRow/ListRow';
+//elements
+export { ChoiceField } from './elements/fields/choiceField/ChoiceField';
+export { SliderField } from './elements/fields/sliderField/SliderField';
+export { DraggableOrder } from './elements/functional/draggableOrder/DraggableOrder';
+//helpers
 export { getIcon } from './helpers/getIcon';
+//structure
+export { ChoiceList } from './structure/input/choiceList/ChoiceList';
+export { FormGenerator } from './structure/input/formGenerator/FormGenerator';
+export { GridIconMenu } from './structure/ui/gridIconMenu/GridIconMenu';
+export { IconList } from './structure/ui/iconList/IconList';
+export { LinkCard } from './structure/ui/linkCard/LinkCard';
 export * from './types';
